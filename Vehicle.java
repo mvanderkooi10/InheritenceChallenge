0bd867e503ac4f88f927e0b9afbcee38669e31d2
mvanderkooi10 <mvanderkooi10@gmail.com>
2017-04-19 17:51:30 -0400

    Initial commit

---
+++ Vehicle.java
@@ -0,0 +1,48 @@
+package com.company;
+
+public class Vehicle {
+    private String name;
+    private String size;
+
+    private int currentVelocity;
+    private int currentDirection;
+
+    public Vehicle(String name, String size) {
+        this.name = name;
+        this.size = size;
+
+        this.currentDirection = 0;
+        this.currentVelocity = 0;
+    }
+
+    public void Steer(int direction){
+        this.currentDirection += direction;
+        System.out.println("Vehicle.steer(): Steering at " + currentDirection + " degrees.");
+    }
+
+    public void Move(int velocity, int direction){
+        currentVelocity = velocity;
+        currentDirection = direction;
+        System.out.println("Vehicle.move(): Moving at " + currentVelocity + " in direction " + currentDirection);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public int getCurrentVelocity() {
+        return currentVelocity;
+    }
+
+    public int getCurrentDirection() {
+        return currentDirection;
+    }
+
+    public void Stop(){
+        this.currentVelocity = 0;
+    }
+}
